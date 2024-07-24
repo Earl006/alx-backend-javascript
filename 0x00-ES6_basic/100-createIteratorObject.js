06d8d810cd2df652f82e8aa72a8d1f98b2a85e9e
Earl006 <earljoe06@gmail.com>
2024-07-24 16:54:29 +0300

    task 13

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+export default function createIteratorObject(report) {
+  const myarray = [];
+  for (const item of Object.values(report.allEmployees)) {
+    myarray.push(...item);
+  }
+
+  return myarray;
+}
