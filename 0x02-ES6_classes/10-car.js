fdab833046bd345bff6abcf4b0fd44fb7e5a3f66
Earl006 <earljoe06@gmail.com>
2024-07-25 13:35:10 +0300

    task 10 correction

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,16 +1,5 @@
-#!/usr/bin/env node
-
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw new TypeError('Brand must be a String');
-    }
-    if (typeof motor !== 'string') {
-      throw new TypeError('Motor must be a string');
-    }
-    if (typeof color !== 'string') {
-      throw new TypeError(color !== 'string');
-    }
     this._brand = brand;
     this._motor = motor;
     this._color = color;
