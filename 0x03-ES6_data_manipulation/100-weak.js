3613ea6cff38d029e1f34c8bb05e06166e80d373
Earl006 <earljoe06@gmail.com>
2024-07-30 23:00:31 +0300

    task 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,10 @@
-export const weakMap = new WeakMap();
+const weakMap = new WeakMap();
 
-export function queryAPI(endPoint) {
-  const total = weakMap.get(endPoint) || 0;
-  weakMap.set(endPoint, total + 1);
-  if (total >= 5) {
-    throw new Error('Endpoint load is high');
-  }
+const queryAPI = (endpoint) => {
+  let total = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, total -= -1);
+  if (total >= 5) throw new Error('Endpoint load is high');
   return total;
-}
+};
+
+export { weakMap, queryAPI };
