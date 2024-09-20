d0ad52627c68bb7004903b63701efdfce3dbd504
Earl006 <earljoe06@gmail.com>
2024-09-20 02:08:01 -0500

    [Fixed]0-console

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(string) {
-    console.log(string);
-  }
-  
-  module.exports = displayMessage;
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Earljoe Kadima <https://github.com/Earl006>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
